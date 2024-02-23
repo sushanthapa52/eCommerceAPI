00a725b51921910d592310519c1edb5c4d451e4c
Dell <sushan.thapa1010@gmail.com>
2024-02-23 18:57:48 -0500

    class lib added

---
+++ eCommerceClassLib/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace eCommerce.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        // Navigation property
+        public List<Product> Products { get; set; }
+
+    }
+}
